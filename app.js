b4becfc5ad9485e6a25f37e63526aeae87f261a0
Nicholas <nicholaswhiffen@hotmail.com>
2021-11-28 21:27:35 -0330

    "plugin fully playable"

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 $("#rps_game").rockPaperSissors({
-    overlay: 'rgba(0, 100, 0, 0.5)',
-    closeButton: {
-        src: "images/close.png",
-        width: "100px",
-        height: "100px"
+    image1: {
+        name: "rock",
+        src: "images/image1.jpeg"
+    },
+    image2: {
+        name: "paper",
+        src: "images/image2.jpeg"
+    },
+    image3: {
+        name: "sissors",
+        src: "images/image3.jpeg"
+    },
+    $gameDiv: {
+        width:  "400px",
+        height : "300px",
+        border: "5px solid black",
+        padding: "50px"
     },
     imageBorder: "15px solid #ffffff",
     borderRadius: "10px",
@@ -13,11 +25,5 @@ $("#rps_game").rockPaperSissors({
         exist: true,
         color: "#ffffff",
         fontSize: "40px"
-    },
-    open: function(){
-        console.log("opened");
-    },
-    close: function(){
-        console.log("closed");
     }
 });
--- jquery.rock_paper_sissors.js
+++ jquery.rock_paper_sissors.js
@@ -1,12 +1,13 @@
+"use strict";
 (function($) {
     $.fn.rockPaperSissors = function(options){
 
         var settings = $.extend({
-        $gameDiv: 'rgba(0, 0, 0, 0.5)',
-            closeButton: {
-                src: null,
-                width: "30px",
-                height: "30px"
+            $gameDiv: {
+                width:  "400px",
+                height : "400px",
+                border: "5px solid black",
+                padding: "100px"
             },
             imageBorder: "5px solid #ffffff",
             borderRadius: "5px",
@@ -16,110 +17,188 @@
                 exist: true,
                 color: "#ffffff",
                 fontSize: "20px"
-            },
-            open: null,
-            close: null
+            }
         }, options)
 
-        /**
-         * Iterating through image gallery
-         */
-        return this.each(function(){
-            /**
-             * Declaring new elements variables
-             */
-            var $gameDiv, $image1, $image2, $imageCaption, html;
-            let gameAmount = 0;
-            setGameStart()
+        const $gameDiv = $("#rps_game");
+        let $image1, $image2, $imageCaption, html;
+        let gameAmount = 0;
+        let gamesPlayed = 0;
+        let wins = 0;
+        let loses = 0;
+        
+        $("#rps_game").on("click", "#gameAmount button", function(event){
+            event.preventDefault();
+            if (parseInt($(this).val()) == 1){
+                gameAmount = 1;
+            }else if(parseInt($(this).val()) == 3){
+                gameAmount = 3;
+            }else if(parseInt($(this).val()) == 5){
+                gameAmount = 5;
+            }
+            console.log(gameAmount);
+            setGameProperties();
             
+        });
 
-            function playGame (){
-                let gamesPlayed = 0;
-                let wins = 0;
-                while(gamesPlayed != gameAmount){
-                    $("button").click(function() {
-                        let opponentChoice = Math.floor(Math.random() * 3) + 1;
-                        let results = compareChoice(parseInt($(this).val(), opponentChoice));
-                        if(results == 1){
-                            wins += 1;
-                            gamesPlayed += 1;
-                        }
-                    });
-                }
+        $("#rps_game").on("click", "#choices button", function(){
+            if(parseInt($(this).val()) == 4){
+                setGameStart();
+            };
+            let opponentChoice = Math.floor(Math.random() * 3) + 1;
+            console.log($(this).val());
+            console.log(opponentChoice);
+            let results = compareChoice(parseInt($(this).val()), opponentChoice);
+            console.log(results);
+            if(results == 1){
+                wins += 1;
+                gamesPlayed += 1;
             }
-
-            function compareChoice(pChoice, oChoice){
-                // Results meaning: 0-Lose 1-Win 2-Tie
-                if(pChoice == 1){
-                    if(oChoice == 1){
-                        return 2;
-                    }else if(oChoice == 2){
-                        return 0;
-                    } else{
-                        return 1;
-                    }
-                }else if(pChoice == 2){
-                    if(oChoice == 2){
-                        return 2;
-                    }else if(oChoice == 3){
-                        return 0;
-                    } else{
-                        return 1;
-                    }
-                }else if(pChoice == 3){
-                    if(oChoice == 3){
-                        return 2;
-                    }else if(oChoice == 1){
-                        return 0;
-                    } else{
-                        return 1;
-                    }
-                }
+            else if(results == 0){
+                gamesPlayed +=1;
+                loses += 1;
             };
+            checkWin();
+                    
+        });
+        
 
-            function setGameProperties(){
-                $gameDiv = $("#rps_game");
-                $gameDiv.empty();
-                html = ('<img src="images\\question.png" id="player1" alt="Item 1">\
-                <img src="images\\question.png" id="player2" alt="Item 2">\
-                <br>\
-                <div id="choices">\
-                    <button type="button" id="1" value="1">Rock</button>\
-                    <button type="button" id="2" value="2">Paper</button>\
-                    <button type="button" id="3" value="3">Sissors</button>\
-                </div>');
-                $gameDiv.append(html);
-                $("img").css({
-                    "width": settings.imageWidth,
-                    "height": settings.imageHeight,
-                    "border": settings.imageBorder,
-                    "border-radius": settings.borderRadius
-                });
-
-                $gameDiv.css({
-                    "text-align": "center",
-                });
-            };
-
-            function setGameStart(){
-                $gameDiv = $("#rps_game");
-                $gameDiv.empty();
-                $gameDiv.append('<button type="button" id="1game" value="1">Best of 1</button>');
-                $gameDiv.append('<button type="button" id="3game" value="3">Best of 3</button>');
-                $gameDiv.append('<button type="button" id="5game" value="5">Best of 5</button>');
-                $("button").click(function() {
-                    if (parseInt($(this).val()) == 1){
-                        gameAmount = 1;
-                    }else if(parseInt($(this).val()) == 3){
-                        gameAmount = 3;
-                    }else if(parseInt($(this).val()) == 5){
-                        gameAmount = 5;
-                    }
-                    console.log(gameAmount);
-                    setGameProperties();
-                });
+        function setGameProperties(){
+            $gameDiv.empty();
+            gamesPlayed = 0;
+            wins = 0;
+            loses = 0;
+            html = (`<div id="player1">\
+            <img src="images\\question.png" id="img1" alt="Item 1">\
+            <br>\
+            <span></span>\
+            </div>\
+            <div id="player2">\
+            <img src="images\\question.png" id="img2" alt="Item 2">\
+            <br>\
+            <span></span>\
+            </div>\
+            <br>\
+            <h3 id="score">0 - 0</h3>\
+            <br>\
+            <div id="choices">\
+                <button type="button" id="1" value="1">${settings.image1.name}</button>\
+                <button type="button" id="2" value="2">${settings.image2.name}</button>\
+                <button type="button" id="3" value="3">${settings.image3.name}</button>\
+            </div>`);
+            $gameDiv.append(html);
+            $("#player1").css({
+                "float": "left",
+                "width": "50%"
                 
+            })
+            $("#player2").css({
+                "float": "left",
+                "width": "50%"
+                
+            })
+            $("img").css({
+                "width": settings.imageWidth,
+                "height": settings.imageHeight,
+                "border": settings.imageBorder,
+                "border-radius": settings.borderRadius
+            });
+
+        };
+        
+        function checkWin(){
+            $("#score").text(`${wins} - ${loses}`);
+            if(wins > (gameAmount/2)){
+                $("#score").text(`YOU WIN!`);
+                $("#choices").empty();
+                $("#choices").append('<button type="button" id="playAgain" value="4">Play Again</button>');
+
+            }else if(loses > (gameAmount/2)){
+                $("#score").text(`YOU LOSE:(`);
+                $("#choices").empty();
+                $("#choices").append('<button type="button" id="playAgain" value="4">Play Again</button>');
+    
             }
+        };
+
+        function setGameStart(){
+            $gameDiv.empty();
+            html = ('<div id="gameAmount">\
+            <button type="button" id="1game" value="1">Best of 1</button>\
+            <button type="button" id="3game" value="3">Best of 3</button>\
+            <button type="button" id="5game" value="5">Best of 5</button>\
+            </div>');
+            $gameDiv.append(html);
+            $gameDiv.css({
+                "width": settings.$gameDiv.width,
+                "height": settings.$gameDiv.height,
+                "border": settings.$gameDiv.border,
+                "padding": settings.$gameDiv.padding,
+                "margin": "auto"
+            });
+            $gameDiv.css({
+                "text-align": "center",
+            });
+        };
+
+        function compareChoice(pChoice, oChoice){
+            // Results meaning: 0-Lose 1-Win 2-Tie
+            if(pChoice == 1){
+                $("#img1").attr("src", settings.image1.src);
+                $("#img1").next().next().text(`${settings.image1.name}`);
+                if(oChoice == 1){
+                    $("#img2").attr("src", settings.image1.src);
+                    $("#img2").next().next().text(`${settings.image1.name}`);
+                    return 2;
+                }else if(oChoice == 2){
+                    $("#img2").attr("src", settings.image2.src);
+                    $("#img2").next().next().text(`${settings.image2.name}`);
+                    return 0;
+                } else{
+                    $("#img2").attr("src", settings.image3.src);
+                    $("#img2").next().next().text(`${settings.image3.name}`);
+                    return 1;
+                }
+            }else if(pChoice == 2){
+                $("#img1").attr("src", settings.image2.src);
+                $("#img1").next().next().text(`${settings.image2.name}`);
+                if(oChoice == 2){
+                    $("#img2").attr("src", settings.image2.src);
+                    $("#img2").next().next().text(`${settings.image2.name}`);
+                    return 2;
+                }else if(oChoice == 3){
+                    $("#img2").attr("src", settings.image3.src);
+                    $("#img2").next().next().text(`${settings.image3.name}`);
+                    return 0;
+                } else{
+                    $("#img2").attr("src", settings.image1.src);
+                    $("#img2").next().next().text(`${settings.image1.name}`);
+                    return 1;
+                }
+            }else if(pChoice == 3){
+                $("#img1").attr("src", settings.image3.src);
+                $("#img1").next().next().text(`${settings.image3.name}`);
+                if(oChoice == 3){
+                    $("#img2").attr("src", settings.image3.src);
+                    $("#img2").next().next().text(`${settings.image3.name}`);
+                    return 2;
+                }else if(oChoice == 1){
+                    $("#img2").attr("src", settings.image1.src);
+                    $("#img2").next().next().text(`${settings.image1.name}`);
+                    return 0;
+                } else{
+                    $("#img2").attr("src", settings.image2.src);
+                    $("#img2").next().next().text(`${settings.image2.name}`);
+                    return 1;
+                }
+            }
+        };
+
+        return this.each(function(){
+            
+            setGameStart();
+            
+            
         })
     }
 }(jQuery))
